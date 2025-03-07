237e375224d533b8410e0d6a19108c659fe4c2cd
Nombre prueba <prueba@hotmail.com>
2025-03-07 17:05:29 +0100

    commit 3

--- pc-2 mostrar imagen/src/main.ts
+++ pc-2 mostrar imagen/src/main.ts
@@ -1,3 +1,12 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const carta = document.getElementById("imagenCarta") as HTMLImageElement;
+const carta2 = document.getElementById("imagenCarta2") as HTMLImageElement;
+
+carta.addEventListener("click", () => {
+    carta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+});
+
+carta2.addEventListener("click", () => {
+    carta2.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+});
